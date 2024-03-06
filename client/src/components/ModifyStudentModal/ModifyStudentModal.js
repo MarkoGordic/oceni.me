7e9aad715c95c17fedd15d0743746d4424dee2eb
Marko Gordić <mgordic22@gmail.com>
2024-03-06 16:27:04 +0100

    added 2 tabs to student modify modal

--- client/src/components/ModifyStudentModal/ModifyStudentModal.js
+++ client/src/components/ModifyStudentModal/ModifyStudentModal.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
+import { toast } from 'react-toastify';
 import './modifyStudentModal.css';
 
-function ModifyStudentModal({ isOpen, onClose, studentId }) {
+function ModifyStudentModal({ isOpen, onClose, studentId, onStudentDeleted  }) {
     const [studentData, setStudentData] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
     const [activeTab, setActiveTab] = useState('studentInfo');
@@ -14,8 +15,12 @@ function ModifyStudentModal({ isOpen, onClose, studentId }) {
 
     async function fetchStudentData(id) {
         setIsLoading(true);
+        setStudentData(null);
         try {
-            const response = await fetch(`http://localhost:8000/students/${id}`);
+            const response = await fetch(`http://localhost:8000/students/get/${id}`);
+            if (!response.ok) {
+                throw new Error('Failed to fetch data');
+            }
             const data = await response.json();
             setStudentData(data);
         } catch (error) {
@@ -25,24 +30,83 @@ function ModifyStudentModal({ isOpen, onClose, studentId }) {
         }
     }
 
+    const deleteStudent = async () => {
+        setIsLoading(true);
+            try {
+                const response = await fetch(`http://localhost:8000/students/delete/${studentId}`);
+                if (response.ok) {
+                    toast.success("Student je uspešno obrisan.");
+                    onClose();
+                    setActiveTab('studentInfo');
+                    setStudentData(null);
+                    onStudentDeleted();
+                } else {
+                    toast.error("Došlo je do greške prilikom brisanja studenta.");
+                }
+            } catch (error) {
+                console.error("Error deleting student:", error);
+                toast.error("Došlo je do greške prilikom brisanja studenta.");
+            } finally {
+                setIsLoading(false);
+            }
+    };
+
     const handleOverlayClick = (e) => {
         onClose();
+        setActiveTab('studentInfo');
+        setStudentData(null);
     };
 
-    const handleModalContentClick = (e) => {
-        e.stopPropagation();
-    };
+    const handleModalContentClick = (e) => e.stopPropagation();
 
-    const handleTabClick = (tabName) => {
-        setActiveTab(tabName);
-    };
+    const handleTabClick = (tabName) => setActiveTab(tabName);
 
     const renderRightColumnContent = () => {
+        if (isLoading) {
+            return <div>Loading...</div>;
+        }
+
+        if (!studentData) {
+            return <div>No data found.</div>;
+        }
+
         switch (activeTab) {
             case 'studentInfo':
-                return <div>Podaci o studentu</div>;
-            case 'manageGroups':
-                return <div>Upravljanje grupama</div>;
+                return (
+                    <div>
+                        <div>
+                            <img className='studentInfo-avatar' src={process.env.PUBLIC_URL + '/student_pfp/' + studentData.id + '.jpg'}></img>
+                            <div className='studentInfo-data-wrap'>
+                                <label className='studentInfo-label' htmlFor="first_name">Ime studenta:</label>
+                                <input className='studentInfo-input' type="text" id="first_name" name="first_name" value={studentData.first_name || ''} />
+
+                                <label className='studentInfo-label' htmlFor="last_name">Prezime studenta:</label>
+                                <input className='studentInfo-input' type="text" id="last_name" name="last_name" value={studentData.last_name || ''} />
+
+                                <label className='studentInfo-label' htmlFor="index_number">Broj indeksa:</label>
+                                <input className='studentInfo-input' type="text" id="index_number" name="index_number" value={studentData.index_number || ''} />
+                            
+                                <label className='studentInfo-label' htmlFor="email">Elektronska pošta:</label>
+                                <input className='studentInfo-input' type="email" id="email" placeholder="primer.tidajem@uns.ac.rs" name="email" value={studentData.email || ''} />
+
+                                <label className='studentInfo-label' htmlFor="password">Postavi novu lozinku:</label>
+                                <input className='studentInfo-input' type="password" id="password" placeholder="MnogoJakaSifra" name="password" />
+
+                            </div>
+                        </div>
+                    </div>
+                );
+                case 'admin':
+                    return (
+                        <div className='modifyModalAdmin-wrap'>
+                            <p style={{ color: '#FF5555', fontWeight: 'bold', marginBottom: '20px' }}>
+                                Upozorenje: Brisanje studenta je trajna akcija koja se ne može poništiti.
+                            </p>
+                            <button onClick={deleteStudent}>
+                                Obriši Studenta
+                            </button>
+                        </div>
+                    );                
             default:
                 return <div>?</div>;
         }
@@ -53,7 +117,7 @@ function ModifyStudentModal({ isOpen, onClose, studentId }) {
     return (
         <div className="modify-student-modal-overlay" onClick={handleOverlayClick}>
             <div className="modify-student-modal-content" onClick={handleModalContentClick}>
-                <div className='modify-student-modal-column-left'>
+            <div className='modify-student-modal-column-left'>
                     <div className={`modify-student-modal-tab ${activeTab === 'studentInfo' ? 'active' : ''}`} onClick={() => handleTabClick('studentInfo')}>
                         <i className='fi fi-bs-user'></i>
                         <p className='sidebar-route-text'>O Studentu</p>
--- client/src/pages/app/App.js
+++ client/src/pages/app/App.js
@@ -1,13 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Sidebar from '../../components/Sidebar/Sidebar';
 import './app.css';
 
 function App() {
+  const [user, setUser] = useState({
+      name: '',
+      email: '',
+      avatar: ''
+  });
+
+  useEffect(() => {
+      const fetchUserData = async () => {
+          try {
+              const response = await fetch('http://localhost:8000/user/me', {
+                  credentials: 'include',
+              });
+              if (!response.ok) {
+                  throw new Error('Network response was not ok');
+              }
+              const userData = await response.json();
+              setUser({
+                  name: userData.first_name + ' ' + userData.last_name,
+                  email: userData.email,
+                  avatar: userData.avatar,
+                  id: userData.id
+              });
+          } catch (error) {
+              console.error('There was a problem fetching user data:', error);
+          }
+      };
+
+      fetchUserData();
+  }, []);
+
 
   return (
     <div className='wrap'>
         <Sidebar />
-        <div className='content'></div>
+        <div className='content'>
+          <h1></h1>
+        </div>
     </div>
   );
 }
