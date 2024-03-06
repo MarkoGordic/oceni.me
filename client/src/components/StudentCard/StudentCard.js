74ad3b99025515be2d9d7f9e24f866acc577e85e
Marko Gordić <mgordic22@gmail.com>
2024-03-06 14:21:35 +0100

    implemented modal for student management without content

--- client/src/components/StudentCard/StudentCard.js
+++ client/src/components/StudentCard/StudentCard.js
@@ -2,18 +2,21 @@ import React from 'react';
 import './studentCard.css';
 
 function StudentCard(props) {
+    const { profile_image, name, email, course, onClick } = props;
+
     return (
-        <div className="student-card">
+        <div className="student-card" onClick={onClick}>
             <div className="student-card-info">
-                <img className='student-card-avatar' src={props.profile_image} alt="Fotografija studenta" />
+                <img className='student-card-avatar' src={profile_image} alt="Student's Avatar" />
                 <div className='student-card-info-details'>
-                    <p className='student-card-full-name'>{props.name}</p>
-                    <p className='student-card-email'>{props.email}</p>
-                    <p className='student-card-course'>{props.course}</p>
+                    <p className='student-card-full-name'>{name}</p>
+                    <p className='student-card-email'>{email}</p>
+                    <p className='student-card-course'>{course}</p>
                 </div>
             </div>
         </div>
     );
 }
+
 
 export default StudentCard;
