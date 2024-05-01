595cefe1e989922d63c6a1a16f497ada7020b64b
Marko Gordić <mgordic22@gmail.com>
2024-05-01 18:10:05 +0200

    Added missed files from last commit

--- server/util/gdb.py
+++ server/util/gdb.py
@@ -2,27 +2,40 @@ import gdb
 import json
 
 instructions = []
+MAX_STEPS = 10000 
 
 class StepAndPrintRegisters(gdb.Command):
-    """Step through the program, instruction by instruction, capturing register states and source code lines, and attempt to dereference registers as strings."""
+    """Step through the program, capturing register states and source code lines, focusing on user-defined functions in .c or .S files."""
 
     def __init__(self):
         super(StepAndPrintRegisters, self).__init__("step_print_regs", gdb.COMMAND_USER)
         self.regs = ["eax", "ebx", "ecx", "edx", "esi", "edi", "esp", "ebp", "eip", "eflags"]
 
     def invoke(self, arg, from_tty):
+        step_count = 0
         try:
             gdb.execute("break main")
             gdb.execute("run < input.txt", to_string=True)
 
-            while True:
+            while step_count < MAX_STEPS:
                 gdb.execute("stepi", to_string=True)
 
-                instruction = gdb.execute("x/i $pc", to_string=True).strip()
                 frame = gdb.selected_frame()
                 sal = frame.find_sal()
+
+                if not sal.symtab or "/lib32/" in sal.symtab.filename:
+                    gdb.execute("finish")
+                    continue
+                
+                source_file = sal.symtab.filename
+                file_extension = source_file.split('.')[-1].lower() if source_file else ""
+
+                if file_extension not in ['c', 's']:
+                    gdb.execute("finish")
+                    continue
+
                 source_line = sal.line
-                source_file = sal.symtab.filename if sal.symtab else "Unknown file"
+                instruction = gdb.execute("x/i $pc", to_string=True).strip()
 
                 registers = {}
                 register_strings = {}
@@ -49,13 +62,20 @@ class StepAndPrintRegisters(gdb.Command):
                         "line": source_line
                     }
                 })
+                
+                step_count += 1
 
         except gdb.error as e:
             print(f"An error occurred: {str(e)}. Program may have finished executing or encountered a breakpoint.")
-        
+
         finally:
             output_path = "<OUTPUT_PATH_PLACEHOLDER>"
             with open(output_path, "w") as outfile:
                 json.dump(instructions, outfile, indent=4)
+            print(f"Execution terminated after {step_count} steps, focusing on .c and .S files.")
+            if step_count == MAX_STEPS:
+                print(f"Reached maximum step count of {MAX_STEPS}.")
+                gdb.execute("quit 99")
+
 
 StepAndPrintRegisters()
--- server/util/run.sh
+++ server/util/run.sh
@@ -1,35 +1,235 @@
-#!/bin/sh
+#!/bin/bash
 
-gcc -g -m32 <TASKNO_PLACEHOLDER>.S -o result
-compile_exit_code=$?
+RED='\e[01;31m'
+GREEN='\e[01;32m'
+BLUE='\e[01;34m'
+NC='\e[00m'
 
-if [ $compile_exit_code -ne 0 ]; then
-    echo "{\"compile\": \"failure\", \"compile_exit_code\": $compile_exit_code, \"output_status\": \"failure\", \"program_output\": \"\", \"expected_output\": \"\", \"program_exit_code\": 0, \"program_exit_status\": \"failure\", \"gdb_exit_code\": 0}" > /output/compile_status<COMPILE_STATUS_TASK_PLACEHOLDER>.json
-    exit 0
+TESTS=(01)
+EXITS=(<EXPECTED_EXIT_CODE_PLACEHOLDER>)
+EMPTY="#"
+MISSING=0
+QUIET=0
+KEEP=0
+SIG=""
+OUT1=/tmp/out1
+
+# input variables
+filename="<TASKNO_PLACEHOLDER>.S"
+
+# config variables
+error="false"
+error_msg=""
+
+compile_status="failure"
+compile_output=""
+
+output_status="failure"
+program_output=""
+program_exit_code=-1
+program_expected_code=<EXPECTED_EXIT_CODE_PLACEHOLDER>
+program_exit_status="failure"
+
+gdb_exit_code=-1
+
+<TESTS_INPUT_OUTPUT_PLACEHOLDER>
+
+for prg in "expect" "sed" "diff" "grep"; do
+    if ! which $prg &>/dev/null; then
+        error_msg="Nedostaje program: $prg"
+        echo "{\"compile\": \"$compile_status\", \"compile_output\": \"$compile_output\", \"error_msg\": \"$error_msg\", \"output_status\": \"$output_status\", \"program_output\": \"$program_output\", \"program_exit_code\": $program_exit_code, \"program_expected_code\": $program_expected_code, \"program_exit_status\": \"$program_exit_status\", \"gdb_exit_code\": $gdb_exit_code}" > /output/compile_status<COMPILE_STATUS_TASK_PLACEHOLDER>.json
+        exit 1
+    fi
+done
+
+# Pocetak kompajliranja
+if [ $filename != "" ] && [ -f $filename ]; then
+    grep ".text" $filename 1>/dev/null 2>/dev/null && (grep ".globl" $filename 1>/dev/null 2>/dev/null || grep ".global" $filename 1>/dev/null 2>/dev/null)
+    if [ $? -ne 0 ]; then
+        error_msg="GREŠKA: Odabrani fajl nije asemblerski program!"
+        echo "{\"compile\": \"$compile_status\", \"compile_output\": \"$compile_output\", \"error_msg\": \"$error_msg\", \"output_status\": \"$output_status\", \"program_output\": \"$program_output\", \"program_exit_code\": $program_exit_code, \"program_expected_code\": $program_expected_code, \"program_exit_status\": \"$program_exit_status\", \"gdb_exit_code\": $gdb_exit_code}" > /output/compile_status<COMPILE_STATUS_TASK_PLACEHOLDER>.json
+        exit 1
+    fi
+
+    GLAVNI=""
+    
+    if [ $(grep -c "main:" $filename) == "0" ]; then
+        if [ -f glavni.c ]; then
+            GLAVNI=glavni.c
+        fi
+    fi
+
+    gcc -g -m32 -o zad $GLAVNI $filename 1>$OUT1 2>&1
+
+    compile_output=$(echo "$compile_output" | base64)
+
+    if [ ! -f zad ]; then
+        error_msg="Greška u kompajliranju ili izvršna datoteka nije kreirana!"
+        echo "{\"compile\": \"failure\", \"compile_output\": \"$compile_output\", \"error_msg\": \"$error_msg\", \"output_status\": \"failure\", \"program_output\": \"\", \"program_exit_code\": 0, \"program_expected_code\": 0, \"program_exit_status\": \"failure\", \"gdb_exit_code\": 0}" > /output/compile_status<COMPILE_STATUS_TASK_PLACEHOLDER>.json
+        rm -f $OUT1
+        exit 1
+    fi
+    
+    if [ $? -ne 0 ]; then
+        error_msg="Greška u kompajliranju!"
+        echo "{\"compile\": \"$compile_status\", \"compile_output\": \"$compile_output\", \"error_msg\": \"$error_msg\", \"output_status\": \"$output_status\", \"program_output\": \"$program_output\", \"program_exit_code\": $program_exit_code, \"program_expected_code\": $program_expected_code, \"program_exit_status\": \"$program_exit_status\", \"gdb_exit_code\": $gdb_exit_code}" > /output/compile_status<COMPILE_STATUS_TASK_PLACEHOLDER>.json
+        rm -f $OUT1
+        exit 1
+    fi
+else
+    if [ $filename != "" ]; then
+        error_msg="Fajl \"$filename\" nije nađen!"
+    fi
+
+    echo "{\"compile\": \"$compile_status\", \"compile_output\": \"$compile_output\", \"error_msg\": \"$error_msg\", \"output_status\": \"$output_status\", \"program_output\": \"$program_output\", \"program_exit_code\": $program_exit_code, \"program_expected_code\": $program_expected_code, \"program_exit_status\": \"$program_exit_status\", \"gdb_exit_code\": $gdb_exit_code}" > /output/compile_status<COMPILE_STATUS_TASK_PLACEHOLDER>.json
+    exit 2
 fi
 
 compile_status="success"
 
-program_output=$(echo "<TASK_INPUT_PLACEHOLDER>" | ./result)
-program_exit_code=$?
-expected_exit_code=<EXPECTED_EXIT_CODE_PLACEHOLDER>
+echo "TESTIRAM PRE GDB"
+echo $filename
 
-<TEST_OUTPUT_PLACEHOLDER>
-expected_output="$OUTP01"
-
-if [ "$program_output" = "$expected_output" ]; then
-    output_status="success"
-else
-    output_status="failure"
-fi
-
-if [ $program_exit_code -eq $expected_exit_code ]; then
-    program_exit_status="success"
-else
-    program_exit_status="failure"
-fi
-
-gdb -q -ex "source gdb.py" -ex "step_print_regs" -ex "quit" result
+# pokrecemo gdb pre provera, u slucaju da bude fork bomba ili prekoracenje resursa, da dobijemo sav izlaz to tog trenutka
+gdb -q -ex "source gdb.py" -ex "step_print_regs" -ex "quit" zad
 gdb_exit_code=$?
 
-echo "{\"compile\": \"$compile_status\", \"compile_exit_code\": $compile_exit_code, \"output_status\": \"$output_status\", \"program_output\": \"$program_output\", \"expected_output\": \"$expected_output\", \"program_exit_code\": $program_exit_code, \"program_exit_status\": \"$program_exit_status\", \"gdb_exit_code\": $gdb_exit_code}" > /output/compile_status<COMPILE_STATUS_TASK_PLACEHOLDER>.json
+if [ $gdb_exit_code -eq 99 ]; then
+    error_msg="Prekoračen je maksimalan broj instrukcija tokom izvršavanja!"
+    echo "{\"compile\": \"$compile_status\", \"compile_output\": \"$compile_output\", \"error_msg\": \"$error_msg\", \"output_status\": \"$output_status\", \"program_output\": \"$program_output\", \"program_exit_code\": $program_exit_code, \"program_expected_code\": $program_expected_code, \"program_exit_status\": \"$program_exit_status\", \"gdb_exit_code\": $gdb_exit_code}" > /output/compile_status<COMPILE_STATUS_TASK_PLACEHOLDER>.json
+    exit 1
+fi
+
+echo "TESTIRAM POSLE GDB"
+
+cat >./run <<EOL
+spawn -noecho [lindex \$argv 0]
+for {set i 1} {\$i < [llength \$argv]} {incr i 1} {
+    sleep 0.5
+    send -- "[lindex \$argv \$i]"
+    send "\r"
+}
+expect eof
+catch wait reason
+set sig [lindex \$reason 5]
+if {\$sig == ""} {
+    set code [lindex \$reason 3]
+} elseif {\$sig == "SIGFPE"} {
+    set code [expr 128+8]
+} elseif {\$sig == "SIGSEGV"} {
+    set code [expr 128+11]
+} elseif {\$sig == "SIGINT"} {
+    set code [expr 128+2]
+} elseif {\$sig == "SIGILL"} {
+    set code [expr 128+4]
+} elseif {\$sig == "SIGKILL"} {
+    set code [expr 128+9]
+} else {
+    set code [expr 128+1]
+}
+exit \$code
+EOL
+
+passed=0
+total=0
+nn=0
+for n in "${TESTS[@]}"; do
+    tcode=${EXITS[$nn]}
+    cor="OUTP$n"
+    eval cor="\$$cor"
+    echo -e "$cor" > out2
+
+    # izlazni kod progrma
+    program_exit_code=$tcode
+    
+    tst="TEST$n"
+    eval tst=\$$tst
+    oldIFS="$IFS"; IFS=$'\n'
+    tst=($tst)
+    IFS="$oldIFS"
+    lin=${#tst[*]}
+
+    for ((l=0; l<lin; l++ )); do
+        if [ "${tst[$l]}" == "$EMPTY" ]; then
+            eval tst[$l]=""
+        fi
+    done
+
+    ok=1
+    expect run ./zad "${tst[@]}" 1>$OUT1 2>&1
+    code=$?
+    sed -i -e '$a\' $OUT1
+    sed -i 's/\x0//g' $OUT1
+    sed -i 's/\xd//g' $OUT1
+    sed -i 's/\x0//g' $OUT1
+    for ((i=1; i<32; i++)); do
+        if [ $i -ne 9 ] && [ $i -ne 10 ] && [ $i -ne 13 ]; then
+            hex=$(printf '%X' $i)
+            sed -i "s/\x$hex/[0x$hex]/g" $OUT1
+        fi
+    done
+
+    for ((i=128; i<256; i++)); do
+        hex=$(printf '%X' $i)
+        sed -i "s/\x$hex/[0x$hex]/g" $OUT1
+    done
+
+    echo "RADIM DIFF"
+
+    # OUT2 sadrzi expected output
+    diff -q -a -w -B $OUT1 out2 1>/dev/null 2>/dev/null
+
+    echo "DIFF ZAVRSEN"
+    echo "DIFF CODE: $?"
+    echo "Izlaz programa: $(cat $OUT1)"
+    echo "Ocekivani izlaz: $cor"
+
+    if [ $? -eq 0 ]; then
+        # outputs are same
+        program_output=$(echo "$OUT1" | base64)
+
+        output_status="success"
+    else
+        # exits are different
+        output_status="failure"
+        ok=0
+    fi
+
+    if [ $code -gt 127 ]; then
+        code=$((code-128))
+        sig=""
+        if [ $code -eq 8 ]; then sig=" (SIGFPE - Floating point exception)"; fi
+        if [ $code -eq 11 ]; then sig=" (SIGSEGV - Invalid memory segment access)"; fi
+        error_msg="Program je vratio Fatal error signal $code$sig!"
+        ok=0
+        SIG="(zbog \e[01;31mexception\e[00m-a) "
+    elif [ $code -eq $tcode ]; then
+        # exit codes are same
+        program_exit_code=$code
+        program_exit_status="success"
+    else
+        # exit codes are different
+        program_exit_code=$code
+        program_exit_status="failure"
+        ok=0
+    fi
+
+    total=$((total + 1))
+
+    if [ $ok -eq 1 ]; then
+        passed=$((passed + 1))
+    fi
+
+    nn=$((nn+1))
+done
+
+if [ $KEEP -eq 0 ]; then
+    rm -f zad run $OUT1 out2 1>/dev/null 2>/dev/null
+else
+    rm -f run 1>/dev/null 2>/dev/null
+fi
+
+compile_output=$(echo "$compile_output" | base64)
+echo "{\"compile\": \"$compile_status\", \"compile_output\": \"$compile_output\", \"error_msg\": \"$error_msg\", \"output_status\": \"$output_status\", \"program_output\": \"$program_output\", \"program_exit_code\": $program_exit_code, \"program_expected_code\": $program_expected_code, \"program_exit_status\": \"$program_exit_status\", \"gdb_exit_code\": $gdb_exit_code}" > /output/compile_status<COMPILE_STATUS_TASK_PLACEHOLDER>.json
+
+exit 0
